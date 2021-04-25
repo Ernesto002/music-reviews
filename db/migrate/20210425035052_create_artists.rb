cc31519ec0a68a15fba0adda780493c967ab484f
ernesto <ernesto002@icloud.com>
2021-04-24 21:22:22 -0700

    add db structure

---
+++ db/migrate/20210425035052_create_artists.rb
@@ -0,0 +1,9 @@
+class CreateArtists < ActiveRecord::Migration[6.0]
+  def change
+    create_table :artists do |t|
+      t.string :name
+      t.belongs_to :medium, foreign_key: true
+      t.timestamps
+    end
+  end
+end
