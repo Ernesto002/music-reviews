443e784d8adfc8aba3599db9096543c6834a49f9
ernesto <ernesto002@icloud.com>
2021-04-28 21:06:18 -0700

    add .included method to usable module

--- app/models/concerns/usable.rb
+++ app/models/concerns/usable.rb
@@ -1,12 +1,18 @@
-module Usable 
-    def build_parent
-        self.build_medium(medium_type: self.class.to_s) unless medium_id
-        if self.instance_of?(Song)
-          self.build_album unless album_id
-          self.album.build_artist unless album.artist_id
-        end
-        if self.instance_of?(Album)
-          self.build_artist unless artist_id
-        end
+module Usable
+  def self.included(base)
+    base.class_eval do 
+      belongs_to :medium
     end
+  end
+
+ def build_parent
+    self.build_medium(medium_type: self.class.to_s) unless medium_id
+    if self.instance_of?(Song)
+      self.build_album unless album_id
+      self.album.build_artist unless album.artist_id
+    end
+    if self.instance_of?(Album)
+      self.build_artist unless artist_id
+    end
+ end
 end
