5748a3c15010a9cd7fa10fe20e190c0254de0a55
ernesto <ernesto002@icloud.com>
2021-04-28 21:21:34 -0700

    fix user model typo

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,6 @@ class User < ApplicationRecord
     has_many :favorites
     has_many :favorited_media, through: :favorites, source: :medium
     validates :username, presence: true, uniqueness: true
-    validates :username, presence: true, uniqueness: true
+    validates :email, presence: true, uniqueness: true
     has_secure_password
 end
