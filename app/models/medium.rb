3a069cec57bc3669eeb80ecb36341ff7279c7a3b
ernesto <ernesto002@icloud.com>
2021-04-28 21:35:23 -0700

    add scope methods to medium

--- app/models/medium.rb
+++ app/models/medium.rb
@@ -3,6 +3,8 @@ class Medium < ApplicationRecord
   has_many :reviewers, through: :reviews, source: :reviewer
   has_many :favorites
   has_many :favoriters, through: :favorites, source: :favoriter
+  scope :reviewed -> { Medium.joins.reviews) }
+  scope :not_reviewed -> { Medium.joins("LEFT JOIN reviews ON media.id = reviews.medium_id WHERE reviews.medium_id IS NULL") }
 
   def element 
     medium_type.constantize.find_by(medium_id: id)
