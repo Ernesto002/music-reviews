bd2bad86b4657e9af703707faf3330cae13eef42
ernesto <ernesto002@icloud.com>
2021-04-30 23:25:20 -0700

    draw routes for controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  resources :users, only: %i[show]
+  root 'static#home'
+  get '/signup' => 'users#new'
+  post '/signup' => 'users#create'
+  
+  get '/login/' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  post '/logout' => 'sessions#destroy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
