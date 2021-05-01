151b09a1d8a4f0ddb025bb03431a23dc654bb618
ernesto <ernesto002@icloud.com>
2021-04-30 23:56:24 -0700

    draw new routes for omniauth

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,9 @@ Rails.application.routes.draw do
   get '/login/' => 'sessions#new'
   post '/login' => 'sessions#create'
   post '/logout' => 'sessions#destroy'
+
+  get '/auth/facebook/callback' => 'sessions#omni_create'
+  get '/auth/google_oauth2/callback' => 'sessions#omni_create'
+  get '/auth/twitter/callback' => 'sessions#omni_create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
