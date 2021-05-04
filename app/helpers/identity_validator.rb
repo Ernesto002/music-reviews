a643a11665a4d89398c3c4514a43ec038d9cf2b0
ernesto <ernesto002@icloud.com>
2021-05-03 22:28:16 -0700

    improve user validation

--- app/helpers/identity_validator.rb
+++ app/helpers/identity_validator.rb
@@ -1,10 +1,17 @@
 class IdentityValidator < ActiveModel::Validator
     def validate(record)
-        if record.username.blank? && record.email.blank? && record.indentities.blank?
-            record.errors[:base] << 'This user must have at least one username, email, or identity.'
+        if record.identities.blank? 
+            if record.username.blank? && record.email.blank? 
+                record.errors[:base] << 'This user must have at least one username, email, or social link'
+            end
+            record.errors[:base] << 'Users must have a password' if record.password_digest.blank?
+            record = enforce_unique(record)
         end
-        if record.identities.blank? && record.password_digest.blank? 
-            record.errors[:base] << 'Users must have a password'
-        end
+    end
+
+    def enforce_unique(record)
+        record.errors[:username] << 'Username is taken' if record.username && User.exists?(username: record.username)
+        record.errors[:email] << 'Email is taken' if record.email && User.exists?(email: record.email)
+        record
     end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,8 @@ class User < ApplicationRecord
     has_many :reviewed_media, through: :reviews, source: :medium
     has_many :favorites
     has_many :favorited_media, through: :favorites, source: :medium
-    validates :username, uniqueness: true
-    validates :email, uniqueness: true
-    validates_with IdentityValidator 
     has_secure_password :validations false
+    validates_with IdentityValidator 
 
     def self.create_with_omni(auth, identity)
         username = auth[:info][:nickname] || auth[:extra][:raw_info][:username]
