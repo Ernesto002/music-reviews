f0d522d77bbe300a9bf6b739ab60fb96f6091cb2
ernesto <ernesto002@icloud.com>
2021-05-03 23:16:28 -0700

    adds favorite validations

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -1,4 +1,7 @@
 class Favorite < ApplicationRecord
   belongs_to :favoriter, class_name: 'User', foreign_key: :true
   belongs_to :medium
+  validates :favoriter, presence: true 
+  validates :medium, presence: true 
+  validates_uniqueness_of :favoriter, scope: :medium
 end
