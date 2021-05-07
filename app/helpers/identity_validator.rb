080d3fe1d54e9b9a7ad40aa9464e139f5ba3b956
ernesto <ernesto002@icloud.com>
2021-05-06 22:06:28 -0700

    lint helpers/refactor identity validator

--- app/helpers/identity_validator.rb
+++ app/helpers/identity_validator.rb
@@ -1,19 +1,19 @@
 class IdentityValidator < ActiveModel::Validator
-    def validate(record)
-        if record.identities.blank? 
-            if record.username.blank? && record.email.blank? 
-                record.errors[:missing_identity] << 'User must have at least one username, email, or social link'
-            end
-            record.errors[:invalid_password] << 'Users must have a password' if record.password_digest.blank?
-        end
-        record = enforce_unique(record)
+  def validate(record)
+    if record.identities.blank?
+      if record.username.blank? && record.email.blank?
+        record.errors[:missing_identity] << 'User must have at least one username, email, or social.'
+      end
+      record.errors[:invalid_password] << 'User must have a password.' if record.password_digest.blank?
     end
-
-    def enforce_unique(record)
-        username_id = User.find_by(username: record.username)&.id
-        email_id = User.find_by(email: record.email)&.id
-        record.errors[:username] << 'Username is taken' if record.username && User.exists?(username: record.username)
-        record.errors[:email] << 'Email is taken' if record.email && User.exists?(email: record.email)
-        record
-    end
+    record = enforce_unique(record)
+  end
+  
+  def enforce_unique(record)
+    username_id = User.find_by(username: record.username)&.id
+    email_id = User.find_by(email: record.email)&.id
+    record.errors[:username] << 'Username is already taken.' if !record.username.blank? && (!username_id.nil? && username_id != record.id)
+    record.errors[:email] << 'Email is already taken.' if !record.email.blank? && (!email_id.nil? && email_id != record.id)
+    record
+  end
 end
