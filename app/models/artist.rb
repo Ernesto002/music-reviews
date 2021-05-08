e05f8fc146fc87d971b66892b06c6f2c49eae03b
ernesto <ernesto002@icloud.com>
2021-05-08 14:29:11 -0700

    improve formatting

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,17 +1,16 @@
 class Artist < ApplicationRecord
-    include Usable
-    include Favoritable
-    include Reviewable
-    has_many :albums
-    has_many :songs, through: :albums
-    before_validation :build_parents
-    validates :medium, presence: true, uniqueness: true
-    validates :title, presence: true
-    validates_uniqueness_of :title
+  include Usable
+  include Favoritable
+  include Reviewable
+  has_many :albums
+  has_many :songs, through: :albums
+  before_validation :build_parents
+  validates :medium, presence: true, uniqueness: true
+  validates :title, presence: true
+  validates_uniqueness_of :title
+  scope :alphabetical, -> {order('title')}
 
-    scope :alphabetical, -> {order('title')}
-  
-    def children
-      albums.blank? ? nil : albums
-    end
+  def children
+    albums.blank? ? nil : albums
+  end
 end
