3e2e2fedfd70d31fd5813ec3c5e55a1d1b0c6f4a
ernesto <ernesto002@icloud.com>
2021-05-07 18:35:54 -0700

    prevents favorite being creating upon review creation

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -13,7 +13,6 @@ class ReviewsController < ApplicationController
     def create
       review = Review.new(review_params)
       review.save
-      Favorite.create(favoriter: review.reviewer, medium: review.medium)
       return redirect_back fallback_location: medium_path(review.medium), notice: review.errors unless review.errors.blank?
       redirect_to medium_path(review.medium)
     end
